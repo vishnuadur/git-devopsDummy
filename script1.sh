50986c32ee70fa912b042e12d4ae5a73b0a051f9
vishnuadur <109670490+vishnuadur@users.noreply.github.com>
2025-09-11 18:03:10 +0530

    remove unwanted thingd

--- script1.sh
+++ script1.sh
@@ -4,7 +4,7 @@
 ping -c 2 google.com > /dev/null 2>&1
 
 if [ $? -eq 0 ]; then
-    echo "✅ Google server is reachable"
+    echo " Google server is reachable"
 else
-    echo "❌ Google server is NOT reachable"
+    echo "Google server is NOT reachable"
 fi

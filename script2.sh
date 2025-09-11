e390dd5d38feb8a18232050f74d783a7b3d6a9cc
vishnuadur <109670490+vishnuadur@users.noreply.github.com>
2025-09-11 18:09:14 +0530

    changes the mail id

--- script2.sh
+++ script2.sh
@@ -10,4 +10,4 @@ echo "Cleanup Report - $(date)" > $LOGFILE
 find "$TARGET_DIR" -type f \( -name "*.log" \) -print -delete >> $LOGFILE 2>&1
 
 
-mail -s "Daily Cleanup Report" vishnu.adur2@gmail.com < $LOGFILE
+mail -s "Daily Cleanup Report" vishnukumar.adur2@gmail.com < $LOGFILE
